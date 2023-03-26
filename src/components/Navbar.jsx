e2b1f370bd83326e733eb35b5a3298163139ab17
Jesse Emana <jesseemana@gmail.com>
2023-03-26 14:27:07 +0200

    fixed conditional rendering

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,34 @@
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { useNavigate } from "react-router-dom"
-import { setLogout } from "../state/recipeSlice"
+import { setLogout } from "../state/appSlice"
 
 import axios from "../api/axios"
 const LOGOUT_URL = '/auth/logout'
 
 
 const Navbar = () => {
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
 
-    
-    const handleLogout = async () => {
-        dispatch(setLogout())
-        await axios.post(LOGOUT_URL)
-        navigate('/')
-    }
-
+  const user = useSelector(state => state.user)
+  
+  const handleLogout = async () => {
+    dispatch(setLogout())
+    await axios.post(LOGOUT_URL)
+    navigate('/')
+  }
+  
+ 
+  if(!user) {
+    return (
+      <div className="p-3 bg-blue-200 shadow-sm flex justify-between">
+        <h1 className="text-2xl font-bold capitalize">foodiesss.</h1>
+      </div>
+    )
+  }
 
   return (
-    <div className="p-3 bg-blue-200 shadow-lg flex justify-between">
+    <div className="p-3 bg-blue-200 shadow-sm flex justify-between">
         <h1 className="text-2xl font-bold capitalize">foodiesss.</h1>
         <button
           className="font-semibold capitalize"

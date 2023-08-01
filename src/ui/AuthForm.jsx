d140ea3f73892100893d3f56c6209d7c4d45ce87
Jesse Emana <jesseemana@gmail.com>
2023-08-01 17:42:08 +0200

    replaced input fields and labels with reuasble InputField component

--- src/ui/AuthForm.jsx
+++ src/ui/AuthForm.jsx
@@ -1,26 +1,25 @@
-import React from 'react'
 import { Link } from 'react-router-dom'
+import InputField from '../components/InputField'
 
 const Form = ({
-    errMsg,
-    formRef,
-    isLogin,
-    isRegister, 
-    handleSubmit, 
-    firstName, 
-    lastName, 
-    email, 
-    password, 
-    password2, 
-    setPageType, 
-    setFirstName, 
-    setLastName, 
-    setEmail, 
-    setPassword, 
-    setPassword2, 
+  errMsg,
+  isLogin,
+  isRegister, 
+  handleSubmit, 
+  firstName, 
+  lastName, 
+  email, 
+  password, 
+  password2, 
+  setPageType, 
+  setFirstName, 
+  setLastName, 
+  setEmail, 
+  setPassword, 
+  setPassword2, 
 }) => {
   return (
-    <div className="flex justify-center items-center py bg-gray-50 h-[96vh]">
+    <div className='flex justify-center items-center py bg-gray-50 h-[96vh]'>
       <form 
         onSubmit={handleSubmit} 
         className='flex flex-col gap-y-4 transition-all w-[300px] md:w-auto px-5 py-4 rounded-sm shadow-lg bg-white'
@@ -30,64 +29,56 @@ const Form = ({
           : <h1 className='text-center text-[#38D6C4] text-xl font-bold uppercase'>login</h1>
         }
         {isRegister && (
-          <div className="flex flex-col gap-y-4 items-start md:flex-row gap-x-4">
-            <div className="flex flex-col w-full">
-              <label htmlFor="firstName" className="text-gray-700 px-1 capitalize">first name:</label>
-                <input
-                  ref={isRegister ? formRef : null}
-                  type="text"
-                  value={firstName}
-                  placeholder="first name"
-                  className="py-3 px-1 text-gray-600 border border-gray-200 rounded-sm outline-[#38D6C4]"
-                  onChange={(e) => setFirstName(e.target.value)}
-                  required
-                />
+          <div className='flex flex-col gap-y-4 items-start md:flex-row gap-x-4'>
+            <div className='flex flex-col w-full'>
+              <InputField 
+                htmlFor={'first name'}
+                label={'first name:'}
+                type='text'
+                value={firstName}
+                placeholder='first name'
+                onChange={(e) => setFirstName(e.target.value)}
+              />
             </div>
                     
-            <div className="flex flex-col w-full">
-              <label htmlFor="lastName" className="text-gray-700 px-1 capitalize">last name:</label>
-              <input
-                type="text"
+            <div className='flex flex-col w-full'>
+              <InputField 
+                htmlFor={'last name'}
+                label={'last name:'}
+                type='text'
                 value={lastName}
-                placeholder="last name"
-                className="py-3 px-1 text-gray-600 border border-gray-200 rounded-sm outline-[#38D6C4]"
+                placeholder='last name'
                 onChange={(e) => setLastName(e.target.value)}
-                required
               />
             </div>
           </div>
         )}
 
-        <label htmlFor="email" className="text-gray-700 px-1 capitalize">email:</label>
-        <input
-          ref={isLogin ? formRef : null}
-          type="text"
+        <InputField 
+          htmlFor={'email'}
+          label={'email:'}
+          type='text'
           value={email}
-          autoComplete="off"
-          placeholder="email"
-          className="py-3 px-1 text-gray-600 border border-gray-200 rounded-sm outline-[#38D6C4]"
+          placeholder='email'
           onChange={(e) => setEmail(e.target.value)}
-          required
         />
             
-        <label htmlFor="password" className="text-gray-700 px-1 capitalize">password:</label>
-        <input
-          type="password"
+        <InputField 
+          htmlFor={'password'}
+          label={'password:'}
+          type='password'
           value={password}
-          placeholder="password"
-          className="py-3 px-1 text-gray-600 border border-gray-200 rounded-sm outline-[#38D6C4]"
+          placeholder='password'
           onChange={(e) => setPassword(e.target.value)}
-          required
         />
-            
-        <label htmlFor="password2" className="text-gray-700 px-1 capitalize">confirm password:</label>
-        <input
-          type="password"
+
+        <InputField 
+          htmlFor={'password'}
+          label={'confirm password:'}
+          type='password'
           value={password2}
-          placeholder="confirm password"
-          className="py-3 px-1 text-gray-600 border border-gray-200 rounded-sm outline-[#38D6C4]"
+          placeholder='confirm password'
           onChange={(e) => setPassword2(e.target.value)}
-          required
         />
                 
         <p className='text-red-500 text-center text-lg font-normal'>{errMsg}</p>
@@ -98,16 +89,16 @@ const Form = ({
                 
         <button
           // disabled={!canSend}
-          type="submit"
-          className={"bg-[#38D6C4] rounded-sm px-10 py-2 text-white uppercase text-md text-center"}>
+          type='submit'
+          className={'bg-[#38D6C4] rounded-sm px-10 py-2 text-white uppercase text-md text-center'}>
           {isLogin ? 'login' : 'create account'}
         </button>
-        <div className="flex gap-1">
-          <div className="text-gray-500 text-sm md:text-[17px]">
-            {isLogin? `Don't have an account?`: 'Already have an account?'}
+        <div className='flex gap-1'>
+          <div className='text-gray-500 text-sm md:text-[17px]'>
+            {isLogin ? `Don't have an account?`: 'Already have an account?'}
           </div>
           <button
-            onClick={() => {setPageType(isLogin ? "register" : "login")}}
+            onClick={() => {setPageType(isLogin ? 'register' : 'login')}}
             className='text-[#38D6C4] text-sm md:text-[17px] underline capitalize'
           >
             {isLogin ? 'create account.' : 'login.'}

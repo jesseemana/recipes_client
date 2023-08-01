a3e3ba8a00beb31f6d3deb2848ee8beca5baf6d9
Jesse Emana <jesseemana@gmail.com>
2023-08-01 18:36:07 +0200

    formatted code

--- src/components/Bookmarks.jsx
+++ src/components/Bookmarks.jsx
@@ -2,26 +2,26 @@ import { useState, useEffect } from 'react'
 import Recipes from '../ui/Recipes'
 import { useSelector } from 'react-redux'
 import Loader from '../ui/Loader'
-
 import axios from '../api/axios'
+
 const BOOKMARKS_URL = '/bookmarks'
 
 const Bookmarks = () => {
   const [loading, setLoading] = useState(true)
-  const [saved, setSaved] = useState([])
+  const [bookmarks, setBookmarks] = useState([])
 
   const userId = useSelector(state => state.user_id)
   const token = useSelector(state => state.access_token)
-
 
   const getBookmarks = async () => {
     try {
       const results = await axios.get(`${BOOKMARKS_URL}/${userId}`, { headers: { 'Authorization' : `Bearer ${token}`}})
       console.log(results.data)
-      setSaved(results?.data)
-      setLoading(false)
+      setBookmarks(results?.data)
     } catch (error) {
       console.error(`AN ERROR OCCURED: ${error.message}`)
+    } finally {
+      setLoading(false)
     }
   }
 
@@ -30,13 +30,19 @@ const Bookmarks = () => {
   }, []) // eslint-disable-line react-hooks/exhaustive-deps
 
   let content
-  if(loading) content = <Loader />
-  else content = <>
-    <div className="max-w-full px-[4%]">
-      <h1 className='capitalize text-gray-600 text-xl md:text-2xl pt-4 font-semibold text-start'>your saved recipes</h1>
-      <Recipes recipes={saved} />
-    </div>
-  </>
+
+  if (loading) {
+    content = <Loader />
+  }
+
+  content = (
+    <>
+      <div className="max-w-full px-[4%]">
+        <h1 className='capitalize text-gray-600 text-xl md:text-2xl pt-4 font-semibold text-start'>your saved recipes</h1>
+        <Recipes recipes={bookmarks} />
+      </div>
+    </>
+  )
 
   return content
 }

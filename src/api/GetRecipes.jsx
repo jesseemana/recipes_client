9baff805dfd9d94639cac4af94c0d9b5d90d50ea
Jesse Emana <jesseemana@gmail.com>
2023-08-01 13:29:56 +0200

    formatted code

--- src/api/GetRecipes.jsx
+++ src/api/GetRecipes.jsx
@@ -7,23 +7,28 @@ import axios from './axios'
 
 const GetRecipes = () => {
   const [recipes, setRecipes] = useState([]) 
-  const [loading, setLoading] = useState(true) 
+  const [loading, setLoading] = useState(false) 
   const [currentPage, setCurrentPage] = useState(1)
   const [totalPages, setTotalPages] = useState(null) 
+
+  const [category, setCategory] = useState('all')
+  const [snack, setSnack] = useState('snack/appetiser')
+  const [breakfast, setBreakFast] = useState('breakfast')
+  const [maincourse, setMainCourse] = useState('main course')
   
   const token = useSelector(state => state.access_token)
 
   const getRecipes = async () => {
     try {
-      const response = await axios.get(`/recipes?page=${currentPage}`,
-        { headers: { 'Authorization': `Bearer ${token}` }}
-      )
+      setLoading(true)
+      const response = await axios.get(`/recipes?page=${currentPage}`, { headers: { 'Authorization': `Bearer ${token}` }})
       const results = await response?.data
-      setLoading(false)
       setRecipes(results.data)
       setTotalPages(results.totalPages)
     } catch (error) {
       console.log(`AN ERROR OCCURED: ${error}`)
+    } finally {
+      setLoading(false)
     }
   }
   
@@ -31,18 +36,32 @@ const GetRecipes = () => {
     getRecipes()
   }, [currentPage]) // eslint-disable-line react-hooks/exhaustive-deps
 
-  console.log(recipes)
+  console.log(category)
   
   if(loading) return <Loader />
   return (
-    <Recipes
-      recipes={recipes}
-      pages={totalPages}
-      currentPage={currentPage}
-      totalPages={totalPages}
-      setCurrentPage={setCurrentPage}
-    />
+    <>
+      <div className='flex pt-4 gap-x-2'>
+        <p className='font-normal text-gray-500 text-lg'>view by category</p>
+        <select 
+          name='' 
+          onChange={(e)=> setCategory(e.target.value)} 
+          className='border text-gray-500 rounded-sm outline-none'
+        >
+          <option value={breakfast}>breakfast</option>
+          <option value={maincourse}>main course</option>
+          <option value={snack}>snack/appetiser</option>
+        </select>
+      </div>
+      <Recipes
+        recipes={recipes}
+        pages={totalPages}
+        currentPage={currentPage}
+        totalPages={totalPages}
+        setCurrentPage={setCurrentPage}
+      />
+    </>
   )
 }
 
-export default GetRecipes
+export default GetRecipes   
--- src/api/GetUsers.jsx
+++ src/api/GetUsers.jsx
@@ -3,18 +3,18 @@ import useSWR from 'swr'
 import { setUsers } from '../state/appSlice'
 
 const GetUsers = () => {
-    const dispatch = useDispatch()
+  const dispatch = useDispatch()
 
-    const token = useSelector(state => state.access_token)
+  const token = useSelector(state => state.access_token)
    
-    const fetcher = (url, token) =>
-        fetch(url, {headers: { 'Authorization': `Bearer ${token}` }}).then(res => res.json())
+  const fetcher = (url, token) =>
+    fetch(url, {headers: { 'Authorization': `Bearer ${token}` }}).then(res => res.json())
     
-    const {isLoading, data, error} = useSWR('http://localhost:8080/api/v1/users', (url) => fetcher(url, token))
+  const {isLoading, data, error} = useSWR('http://localhost:8080/api/v1/users', (url) => fetcher(url, token))
 
-    if(isLoading) return <div>Loading...</div>
-    if(error) return <div>Couldn't get data, refresh page</div>
-    dispatch(setUsers({user: data}))
+  if (isLoading) return <div>Loading...</div>
+  if (error) return <div>Couldn't get data, refresh page</div>
+  dispatch(setUsers({user: data}))
 }
 
 export default GetUsers

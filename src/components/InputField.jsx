1bead21b3d8e073a4c45fe0715446eb7915e9b52
Jesse Emana <jesseemana@gmail.com>
2023-08-01 16:51:04 +0200

    created reusable input component

---
+++ src/components/InputField.jsx
@@ -0,0 +1,19 @@
+const InputField = ({ref, type, text, autoComplete, placeholder, onChange, htmlFor, label}) => {
+  return (
+    <>
+      <label htmlFor={htmlFor} className='text-gray-700 px-1 capitalize'>{label}</label>
+      <input
+        ref={ref} 
+        type={type} 
+        text={text}
+        onChange={onChange} 
+        placeholder={placeholder}
+        autoComplete={autoComplete}
+        className='py-3 px-1 text-gray-600 border border-gray-200 rounded-sm outline-[#38D6C4]'
+        required
+      />
+    </>
+  )
+}
+
+export default InputField

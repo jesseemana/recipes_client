a93a21077eccf3f2387983bb33b8c625700a5d6d
Jesse Emana <jesseemana@gmail.com>
2023-08-04 18:53:19 +0200

    changed text color

--- src/components/Navbar/MenuItem.jsx
+++ src/components/Navbar/MenuItem.jsx
@@ -2,7 +2,7 @@ const MenuItem = ({onClick, label, icon}) => {
   return (
     <div 
       onClick={onClick} 
-      className='px-4 py-3 hover:bg-neutral-100 transition font-normal flex items-center gap-1'
+      className='px-4 py-3 hover:bg-neutral-100 transition font-normal flex items-center gap-1 text-gray-600'
     >
       <div className='text-lg'>{icon}</div>
       <p className='text-md'>{label}</p>

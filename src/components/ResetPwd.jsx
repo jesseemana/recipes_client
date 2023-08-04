edd1c1297ef62b8dfb89379440ddebb5f4cf723c
Jesse Emana <jesseemana@gmail.com>
2023-08-04 13:41:13 +0200

    fixed page height

--- src/components/ResetPwd.jsx
+++ src/components/ResetPwd.jsx
@@ -37,7 +37,7 @@ const ResetPwd = () => {
   }
 
   return (
-    <div className='max-w-full px-[4%] py-5 bg-gray-50 flex items-center justify-center h-[90vh]'>
+    <div className='max-w-full px-[4%] py-5 bg-gray-50 flex items-center justify-center h-[100vh]'>
       <div className='bg-white px-2 py-4 shadow-md'>
         <h1 className='text-center capitalize text-lg md:text-2xl font-semibold text-[#38D6C4] py-1'>reset your password</h1>
         <form type='submit' className='flex flex-col w-[280px] md:w-[320px] gap-y-2 py-2 px-1'>

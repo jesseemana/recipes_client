67d054f99e535d8070dc436deb0a35ffa85f6ce0
Jesse Emana <jesseemana@gmail.com>
2023-08-05 21:40:27 +0200

    removed chakra imports

--- src/main.jsx
+++ src/main.jsx
@@ -2,22 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import { ChakraProvider } from '@chakra-ui/react'
-
 
 import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { configureStore } from "@reduxjs/toolkit"
 import appReducer from './state/appSlice'
 import {
-    persistStore,
-    persistReducer,
-    FLUSH,
-    REHYDRATE,
-    PAUSE,
-    PERSIST,
-    PURGE,
-    REGISTER
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER
 } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 import { PersistGate } from 'redux-persist/integration/react'
@@ -27,13 +25,13 @@ const persistedReducer = persistReducer(persistConfig, appReducer)
 
 
 const store = configureStore({
-    reducer: persistedReducer,
-    middleware: (getDefaultMiddleware) =>
-      getDefaultMiddleware({
-        serializableCheck: {
-          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
-        },
-      })
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+      },
+    })
 })
 
 
@@ -41,12 +39,10 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistStore(store)}>
-        <ChakraProvider>
-          <Router>
-            <App />
-          </Router>
-        </ChakraProvider>
+        <Router>
+          <App />
+        </Router>
       </PersistGate>
     </Provider>
-  </React.StrictMode>,
+  </React.StrictMode>
 )

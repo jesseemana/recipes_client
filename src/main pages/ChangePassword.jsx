79f6990be9175528c5af715a147bf04e3ce6ed9c
Jesse Emana <jesseemana@gmail.com>
2023-08-05 22:34:50 +0200

    finished reset password page

--- src/main pages/ChangePassword.jsx
+++ src/main pages/ChangePassword.jsx
@@ -1,15 +1,67 @@
 import { useState } from 'react'
-import useDocumentTitle from "../hooks/useDocumentTitle"
+import { toast } from 'react-hot-toast'
+import { useParams } from 'react-router-dom'
+import InputField from '../components/InputField'
+import useDocumentTitle from '../hooks/useDocumentTitle'
+import axios from '../api/axios'
+
 
 const ChangePassword = () => {
+  const {id, token} = useParams()
+
   const [password, setPassword] = useState('')
   const [confirmPassword, setConfirmPassword] = useState('')
 
   useDocumentTitle('Change Password')
+
+  async function resetPwd() {
+    try {
+      await axios.patch(`reset/:${id}/:${token}`)
+    } catch (error) {
+      console.log(`AN ERROR OCCURED: ${error}`)
+    }
+  }
+
+  function toggleChangePassword(e) {
+    e.preventDefault()
+    toast.promise(resetPwd(), {
+      loading: 'updating...',
+      success: 'password updated',
+      error: `couldn't update password` 
+    })
+  }
   
   return (
-    <div>ChangePassword</div>
+    <div className='max-w-full px-[4%] bg-gray-50 flex items-center justify-center h-[100vh]'>
+      <div className='bg-white px-2 py-4 shadow-md rounded-md'>
+        <h1 className='text-center capitalize text-lg md:text-2xl font-semibold text-[#38D6C4] py-1'>enter new password</h1>
+        <form type='submit' className='flex flex-col w-[280px] md:w-[320px] gap-y-2 py-2 px-1'>
+          <InputField
+            htmlFor={'password'}
+            label={'password:'}
+            type='password' 
+            value={password}
+            placeholder='password' 
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <InputField
+            htmlFor={'confirm password'}
+            label={'confirm password:'}
+            type='password' 
+            value={confirmPassword}
+            placeholder='confirm password' 
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+          <button 
+            onClick={toggleChangePassword} 
+            className='bg-[#38D6C4] rounded-sm py-1 text-white uppercase text-sm md:text-[17px] text-center font-medium'
+          >
+            reset password
+          </button>
+        </form>
+      </div>
+    </div>
   )
 }
 
-export default ChangePassword
+export default ChangePassword 
--- src/main pages/ResetPwd.jsx
+++ src/main pages/ResetPwd.jsx
@@ -4,31 +4,22 @@ import InputField from '../components/InputField'
 import useDocumentTitle from '../hooks/useDocumentTitle'
 import axios from '../api/axios'
 
-const RESET_URL = '/reset'
-
 const ResetPwd = () => {  
   const [email, setEmail] = useState('')
-
+  
   useDocumentTitle('Reset Password')
 
-  async function resetPwd() {
-    // try {
-    //   const results = await axios.post(`${RESET_URL}`)
-    //   console.log(results.data)
-    // } catch (error) {
-    //   console.log(`AN ERROR OCCURED: ${error}`)
-    // }
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve('email sent')
-        setEmail('')
-      }, 3000 )
-    })
+  async function sendEmail() {
+    try {
+      await axios.post(`/reset`)
+    } catch (error) {
+      console.log(`AN ERROR OCCURED: ${error}`)
+    }
   }
 
-  function toggleReset(e) {
+  function toggleSendEmail(e) {
     e.preventDefault()
-    toast.promise(resetPwd(), {
+    toast.promise(sendEmail(), {
       loading: 'sending...',
       success: 'email sent',
       error: `couldn't send email` 
@@ -36,8 +27,8 @@ const ResetPwd = () => {
   }
 
   return (
-    <div className='max-w-full px-[4%] py-5 bg-gray-50 flex items-center justify-center h-[100vh]'>
-      <div className='bg-white px-2 py-4 shadow-md'>
+    <div className='max-w-full px-[4%] bg-gray-50 flex items-center justify-center h-[100vh]'>
+      <div className='bg-white px-2 py-4 shadow-md rounded-md'>
         <h1 className='text-center capitalize text-lg md:text-2xl font-semibold text-[#38D6C4] py-1'>reset your password</h1>
         <form type='submit' className='flex flex-col w-[280px] md:w-[320px] gap-y-2 py-2 px-1'>
           <InputField 
@@ -50,16 +41,16 @@ const ResetPwd = () => {
           />
           {email ? 
             <button 
-              onClick={toggleReset} 
+              onClick={toggleSendEmail} 
               className='bg-[#38D6C4] rounded-sm py-1 text-white uppercase text-sm md:text-[17px] text-center font-medium'
             >
-              reset password
+              send email
             </button> : 
             <button 
               disabled 
               className='bg-[#8feee3] rounded-sm py-1 text-white uppercase text-sm md:text-[17px] text-center font-medium cursor-not-allowed'
             >
-              reset password
+              send email
             </button>
           }
         </form>

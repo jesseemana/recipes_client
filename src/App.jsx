36082151689ec100169ffa8ac8b5f66a76ea4c29
Jesse Emana <jesseemana@gmail.com>
2023-08-09 16:31:08 +0200

    created RequireAuth component to protect routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useSelector } from 'react-redux'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
 import Navbar from './components/Navbar/Navbar'
 import CreateRecipe from './main pages/CreateRecipe'
 import ResetPwd from './main pages/ResetPwd'
 import AuthUser from './main pages/AuthUser'
+import RequireAuth from './components/RequireAuth'
 import Bookmarks from './main pages/Bookmarks'
 import NotFound from './main pages/NotFound'
 import Loader from './components/ui/Loader'
@@ -20,24 +20,28 @@ const Profile = lazy(() => import('./main pages/Profile'))
 
 
 function App(){ 
-  const isAuth = Boolean(useSelector((state) => state.access_token))
-
   return (
     <>
       <Navbar />
       <ErrorBoundary FallbackComponent={ErrorFallback}>
         <Routes>
           <Route path='/' element={<Navigate to='/feed'/>} />
-          <Route path='/auth' element={isAuth ? <Navigate to='/feed'/> : <AuthUser />} />
-          <Route path='/reset' element={<ResetPwd />} />
-          <Route path='/reset-password/:id/:token' element={<ChangePassword />} />
+          <Route path='/auth' element={<AuthUser />} />
           <Route path='/feed' element={<Suspense fallback={<Loader />}> <Home /> </Suspense>} />
-          <Route path='/bookmarks' element={isAuth ? <Suspense fallback={<Loader />}> <Bookmarks /> </Suspense> : <Navigate to='/auth'/>} />
+          <Route path='/addrecipe' element={<CreateRecipe/>} />
           <Route path='/recipe/:id' element={<Suspense fallback={<Loader />}> <Recipe /> </Suspense>} />
           <Route path='/user/:id' element={<Suspense fallback={<Loader />}> <UserRecipes /> </Suspense>} />
-          <Route path='/profile/:id' element={isAuth ? <Suspense fallback={<Loader />}> <Profile /> </Suspense> : <Navigate to='/'/>} />
-          <Route path='/newrecipe' element={<CreateRecipe/>} />
-          {/* <Route path='/newrecipe' element={isAuth ? <CreateRecipe/> : <Navigate to='/auth'/>} /> */}
+          <Route path='/reset' element={<ResetPwd />} />
+          <Route path='/reset-password/:id/:token' element={<ChangePassword />} />
+
+          {/* PROTECTED ROUTES */}
+          <Route element={<RequireAuth />}>
+            <Route path='/auth' element={<Navigate to='/feed'/>} />
+            <Route path='/bookmarks' element={<Suspense fallback={<Loader />}> <Bookmarks /> </Suspense>} />
+            <Route path='/profile/:id' element={<Suspense fallback={<Loader />}> <Profile /> </Suspense>} />
+            <Route path='/newrecipe' element={<CreateRecipe/>} />
+          </Route>
+          
           <Route path='*' element={<NotFound />} />
         </Routes>
       </ErrorBoundary>

73d917b71895cefa4946ecd579abdaa90ff9c1fd
Jesse Emana <jesseemana@gmail.com>
2023-08-19 11:31:31 +0200

    changed icons

--- src/components/Navbar/MobileNav.jsx
+++ src/components/Navbar/MobileNav.jsx
@@ -1,30 +1,30 @@
 import Menu from './Menu'
 import Content from '../Content'
-import { AiFillHome, AiOutlineUser } from 'react-icons/ai'
-import { BsBookmark } from 'react-icons/bs'
+import { AiOutlineUser } from 'react-icons/ai'
 import { MdOutlineAddBox } from 'react-icons/md'
-import { BiLogOut, BiLogIn } from 'react-icons/bi'
+import { BsBookmark, BsHouse } from 'react-icons/bs'
+import { CgLogOut, CgLogIn  } from 'react-icons/cg'
 
 const MobileNav = ({ user, handleLogout }) => {
   return (
     <Content>
-      <div className='lg:hidden'>
+      <nav className='lg:hidden'>
         <div className='fixed left-0 right-0 top-0  bg-white p-4 shadow-sm'>
           <h1 className='text-md md:text-2xl text-[#38D6C4] text-center font-normal uppercase'>logooo</h1>
         </div>
-        <div className='fixed left-0 right-0 bottom-0 bg-white p-4 shadow-lg border'>
+        <div className='fixed left-0 right-0 bottom-0 bg-white p-4 border'>
           <div className='flex justify-between'>
-            <Menu route={'/feed'} icon={<AiFillHome />} />
+            <Menu route={'/feed'} icon={<BsHouse />} />
             <Menu route={'/bookmarks'} icon={<BsBookmark />} />
             <Menu route={'/addrecipe'} icon={<MdOutlineAddBox />} />
             <Menu route={'/profile/:id'} icon={<AiOutlineUser />} />
             {!user 
-              ? (<Menu route={'/auth'} icon={<BiLogIn />} />)
-              : (<Menu onClick={handleLogout} icon={<BiLogOut />} />) 
+              ? (<Menu route={'/auth'} icon={<CgLogIn />} />)
+              : (<Menu onClick={handleLogout} icon={<CgLogOut />} />) 
             }
           </div>
         </div>
-      </div>
+      </nav>
     </Content>
   )
 }

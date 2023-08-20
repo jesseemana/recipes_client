9e1a81700b03a3a24a8bc50d112feba36df30482
Jesse Emana <jesseemana@gmail.com>
2023-08-20 20:32:07 +0200

    removed redux stuff

--- src/main.jsx
+++ src/main.jsx
@@ -3,50 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-import { BrowserRouter as Router } from 'react-router-dom'
 import { AuthProvider } from './context/AuthProvider'
-
-import { Provider } from 'react-redux'
-import { configureStore } from "@reduxjs/toolkit"
-import appReducer from './state/appSlice'
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER
-} from 'redux-persist'
-import storage from 'redux-persist/lib/storage'
-import { PersistGate } from 'redux-persist/integration/react'
-
-const persistConfig = { key: 'root', storage, version: 1 };
-const persistedReducer = persistReducer(persistConfig, appReducer)
-
-
-const store = configureStore({
-  reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
-      },
-    })
-})
-
+import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistStore(store)}>
-        <AuthProvider>
-          <Router>
-            <App />
-          </Router>
-        </AuthProvider>
-      </PersistGate>
-    </Provider>
+    <AuthProvider>
+      <Router>
+        <App />
+      </Router>
+    </AuthProvider>
   </React.StrictMode>
 )

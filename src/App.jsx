c5af2861bb4a0947d83e42fdb49f51316514203a
Jesse Emana <jesseemana@gmail.com>
2023-08-20 19:15:11 +0200

    removed lazy loading components

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
+import Loader from './components/Loader'
 import Navbar from './components/Navbar/Navbar'
-import CreateRecipe from './main pages/CreateRecipe'
-import ResetPwd from './main pages/ResetPwd'
-import AuthUser from './main pages/AuthUser'
+import Home from './mainpages/Home'
+import Recipe from './mainpages/Recipe'
+import Profile from './mainpages/Profile'
+import NotFound from './mainpages/NotFound'
+import ResetPwd from './mainpages/ResetPwd'
+import AuthUser from './mainpages/AuthUser'
+import Bookmarks from './mainpages/Bookmarks'
+import UserRecipes from './mainpages/UserRecipes'
 import RequireAuth from './components/RequireAuth'
-import Bookmarks from './main pages/Bookmarks'
-import NotFound from './main pages/NotFound'
-import Loader from './components/ui/Loader'
-import ChangePassword from './main pages/ChangePassword'
+import CreateRecipe from './mainpages/CreateRecipe'
+import ChangePassword from './mainpages/ChangePassword'
 
 import { lazy, Suspense } from 'react'
 import { ErrorBoundary } from 'react-error-boundary'
 import ErrorFallback from './components/ErrorFallback'
-const Home = lazy(() => import('./main pages/Home'))
-const Recipe = lazy(() => import('./main pages/Recipe'))
-const UserRecipes = lazy(() => import('./main pages/UserRecipes'))
-const Profile = lazy(() => import('./main pages/Profile'))
+// const Home = lazy(() => import('./main pages/Home'))
+// const Recipe = lazy(() => import('./mainpages/Recipe'))
+// const UserRecipes = lazy(() => import('./mainpages/UserRecipes'))
+// const Profile = lazy(() => import('./mainpages/Profile'))
 
 
 function App(){ 
@@ -27,20 +31,20 @@ function App(){
         <Routes>
           <Route path='/' element={<Navigate to='/feed'/>} />
           <Route path='/auth' element={<AuthUser />} />
-          <Route path='/feed' element={<Suspense fallback={<Loader />}> <Home /> </Suspense>} />
-          <Route path='/recipe/:id' element={<Suspense fallback={<Loader />}> <Recipe /> </Suspense>} />
+          <Route path='/feed' element={<Home />} />
+          <Route path='/recipe/:id' element={<Recipe />} />
           <Route path='/reset' element={<ResetPwd />} />
           <Route path='/reset-password/:id/:token' element={<ChangePassword />} />
 
           {/* PROTECTED ROUTES */}
           <Route element={<RequireAuth />}>
             <Route path='/auth' element={<Navigate to='/feed'/>} />
-            <Route path='/addrecipe' element={<CreateRecipe/>} />
-            <Route path='/bookmarks' element={<Suspense fallback={<Loader />}> <Bookmarks /> </Suspense>} />
-            <Route path='/user/:id' element={<Suspense fallback={<Loader />}> <UserRecipes /> </Suspense>} />
-            <Route path='/profile/:id' element={<Suspense fallback={<Loader />}> <Profile /> </Suspense>} />
+            <Route path='/create' element={<CreateRecipe />} />
+            <Route path='/bookmarks' element={<Bookmarks />} />
+            <Route path='/user/:id' element={<UserRecipes />} />
+            <Route path='/profile/:id' element={<Profile />} />
           </Route>
-          
+
           <Route path='*' element={<NotFound />} />
         </Routes>
       </ErrorBoundary>
@@ -50,3 +54,7 @@ function App(){
 }
 
 export default App  
+
+{/* <Route path='/bookmarks' element={<Suspense fallback={<Loader />}> <Bookmarks /> </Suspense>} />
+<Route path='/user/:id' element={<Suspense fallback={<Loader />}> <UserRecipes /> </Suspense>} />
+<Route path='/profile/:id' element={<Suspense fallback={<Loader />}> <Profile /> </Suspense>} /> */}

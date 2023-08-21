90dd26b048304586ef7a21b4e9c813943faf32e4
Jesse Emana <jesseemana@gmail.com>
2023-08-21 18:36:24 +0200

    fixed padding across x axis

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,14 +1,34 @@
 import RecipeCard from './RecipeCard'
 import Pagination from './Pagination'
 import useDocumentTitle from '../hooks/useDocumentTitle'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
 
-const Feed = ({recipes, pages, currentPage, setCurrentPage}) => {
+const Feed = ({user, pages, currentPage, setCurrentPage}) => {
+  const [recipes, setRecipes] = useState([])
+
   useDocumentTitle('Feed')
 
+  useEffect(() => {
+    async function fetchRecipes() {
+      const response = await axios.get('http://localhost:3030/recipes')
+      const results = await response.data
+      setRecipes(results)
+      console.log(results)
+    }
+    fetchRecipes()
+  }, [])
+
   return (
-    <div className='pt-20 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8'>
-      <RecipeCard data={recipes} />
-      {recipes?.length < 12 && (
+    <div className='py-20 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8'>
+      {recipes.map(recipe => (
+        <RecipeCard 
+          data={recipe}
+          user={user}
+          key={recipe.id}
+        />
+      ))}
+      {recipes?.length > 16 && (
         <Pagination 
           pages={pages} 
           currentPage={currentPage} 

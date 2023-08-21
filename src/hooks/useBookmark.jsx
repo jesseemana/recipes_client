b0db16c5f1a1d0faceb0820c865cb995b21a671c
Jesse Emana <jesseemana@gmail.com>
2023-08-21 19:07:33 +0200

    removed toast promises for bookmarking

--- src/hooks/useBookmark.jsx
+++ src/hooks/useBookmark.jsx
@@ -1,39 +1,41 @@
-import { useMemo } from 'react'
 import axios from '../api/axios'
-import useAuth from './useAuth'
+import { useMemo } from 'react'
+import { toast } from 'react-hot-toast'
 
-const useBookmark = (id) => {
-  const { auth } = useAuth()
-
-  const token = auth.token
-  const userId = auth.user._id
+const useBookmark = ({id, auth}) => {
+  const userId = auth?.user._id
+  const token = auth?.access_token
 
   const bookmarked = useMemo(() => {
-    return auth.user.Bookmarks.includes(id)
-  }, [])
+    return auth?.user.bookmarks.includes(id)
+  }, [id, auth])
 
-  const handleBookmark = async () => {
+  const toggleBookmark = async () => {
     try {
       if (bookmarked) {
         await axios.delete(`/bookmarks/${id}/${userId}`, {
           headers: {'Authorization': `Bearer ${token}`}
         })
+        toast.success('Bookmark removed')
       } else {
         await axios.post(`/bookmarks/${id}/${userId}`, {
           headers: {'Authorization': `Bearer ${token}`}
         })
+        toast.success('Bookmarked')
       }
+      // refresh page here
     } catch (error) {
       let errorMessage = 'Something went wrong: '
       if (error instanceof Error)
         errorMessage += error
       console.log(error)
+      toast.error('Failed to bookmark' )
     }
   }
 
   return {
     bookmarked,
-    handleBookmark
+    toggleBookmark
   }
 }
 

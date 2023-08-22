0e8a7efd7f2b55edb233409d31a2be1cbb5eb9da
Jesse Emana <jesseemana@gmail.com>
2023-08-22 15:28:53 +0200

    added styling for deleting state

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,14 +4,15 @@ const Button = ({ disabled, onClick, label, type}) => {
       type={type}
       disabled={disabled}
       onClick={onClick} 
-      className={type === 'submit' 
-        ? 'bg-[#38D6C4] rounded-sm py-2 text-white uppercase text-sm md:text-[17px] text-center font-normal disabled:opacity-70 disabled:cursor-not-allowed'
-        : 'text-[#38D6C4] text-sm md:text-[17px] underline capitalize cursor-pointer disabled:cursor-not-allowed'
-      }
+      className={`text-[#38D6C4] text-sm md:text-[17px] underline capitalize cursor-pointer disabled:opacity-80 disabled:cursor-not-allowed
+        ${type === 'submit' 
+        ? 'bg-[#38D6C4] text-white uppercase rounded-sm py-2 text-sm text-center' 
+        : 'bg-rose-500 text-white uppercase rounded-sm no-underline py-1' }
+      `}
     >
       {label}
     </button>
   )
 }
 
-export default Button
+export default Button 

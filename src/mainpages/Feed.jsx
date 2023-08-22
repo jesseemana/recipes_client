9ace91001d740d34375ad8005859d7061ab7d36b
Jesse Emana <jesseemana@gmail.com>
2023-08-22 15:27:12 +0200

    moved feed component into pages folder

---
+++ src/mainpages/Feed.jsx
@@ -0,0 +1,57 @@
+import RecipeCard from '../components/RecipeCard'
+import Pagination from '../components/Pagination'
+import useDocumentTitle from '../hooks/useDocumentTitle'
+import SkeletonCard from '../components/Loaders/Skeleton/SkeletonCard'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const Feed = ({user, pages, currentPage, setCurrentPage}) => {
+  useDocumentTitle('Feed')
+
+  const [recipes, setRecipes] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    async function fetchRecipes() {
+      setLoading(true)
+      try {
+        const response = await axios.get('http://localhost:3030/recipes')
+        if (response.data)
+          setRecipes(response.data)
+      } catch (error) {
+        
+      }  finally {
+        setLoading(false)
+      }
+    }
+
+    fetchRecipes()
+  }, [])
+
+  if (loading) {
+    return [...Array(10).keys()].map(i => {
+      return <SkeletonCard key={i} />
+    })
+  }
+
+  return (
+    <div className='py-20 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8'>
+      {recipes.map(recipe => (
+        <RecipeCard 
+          data={recipe}
+          user={user}
+          key={recipe.id}
+        />
+      ))}
+      {recipes?.length > 16 && (
+        <Pagination 
+          pages={pages} 
+          currentPage={currentPage} 
+          setCurrentPage={setCurrentPage} 
+        />
+      )}
+    </div>
+  )
+}
+
+export default Feed 

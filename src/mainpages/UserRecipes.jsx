1456fca4e4a0118f9e858d5ff393227f2be134a8
Jesse Emana <jesseemana@gmail.com>
2023-08-23 13:19:41 +0200

    changed page layout and view

--- src/mainpages/UserRecipes.jsx
+++ src/mainpages/UserRecipes.jsx
@@ -1,12 +1,12 @@
 import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import axios from '../api/axios'
+import useAuth from '../hooks/useAuth'
+import Header from '../components/Header'
+import Content from '../components/Content'
 import RecipeCard from '../components/RecipeCard'
 import Loader from '../components/Loaders/Loader'
 import useDocumentTitle from '../hooks/useDocumentTitle'
-import useAuth from '../hooks/useAuth'
-
-const RECIPE_URL = '/recipes/user'
 
 const UserRecipes = () =>{
   const { id } = useParams() // in next, user router from navigation/router to get userId
@@ -16,24 +16,29 @@ const UserRecipes = () =>{
   const [recipes, setRecipes] = useState([])
   const [loading, setLoading] = useState(true)
 
-  const token = auth.access_token
+  const token = auth?.access_token
 
   useDocumentTitle(loading ? 'Loading...': `${owner}'s Recipes`)
 
-  const getUserRecipes = async () => {
-    try {
-      const response = await axios.get(`${RECIPE_URL}/${id}`, { headers: { 'Authorization': `Bearer ${token}`}})
-      const results = await response?.data
-      setRecipes(results.recipes)
-      setOwner(results.full_name)
-    } catch (error) {
-      console.error(`An error occured: ${error.message}`)
-    } finally {
-      setLoading(false)
-    }
-  }
-
   useEffect(() => {
+    const getUserRecipes = async () => {
+      try {
+        const response = await axios.get(`/recipes/user/${id}`, { 
+          headers: { 'Authorization': `Bearer ${token}`}
+        })
+        if (response?.data)
+          setRecipes(response.data.recipes)
+          setOwner(response.data.full_name)
+      } catch (error) {
+          let errorMessage = 'Something went wrong: '
+          if (error instanceof Error)
+            errorMessage += error
+          console.log(errorMessage)
+      } finally {
+        setLoading(false)
+      }
+    }
+
     getUserRecipes()
   }, []) // eslint-disable-line react-hooks/exhaustive-deps
 
@@ -41,21 +46,28 @@ const UserRecipes = () =>{
 
   if (loading) {
     content = (
-      <div className='max-w-full px-[4%] border flex justify-center items-center h-[90vh]'>
-        <Loader />
-      </div>
+      <Loader />
     )
   }
 
-  else content = (
-    <div className='max-w-full px-[4%]'>
-      <h1 className=''>user recipes</h1>
-      <p className='capitalize text-center pt-7'>more recipes by {owner}</p>
-      <RecipesCard recipes={recipes} />
-    </div>
+  content = (
+    <Content>
+      <Header 
+        title='more recipes' 
+        subtitle={`View more recipes by ${'this user'}`}
+      />
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8'>
+        {recipes.map(recipe =>(
+          <RecipeCard 
+            data={recipe} 
+            key={recipe.id} 
+          />
+        ))}
+      </div>
+    </Content>
   )
     
   return content
 }
 
-export default UserRecipes
+export default UserRecipes  

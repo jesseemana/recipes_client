ca4620dcbeef5bba135404f5505494f700e6e451
Jesse Emana <jesseemana@gmail.com>
2023-08-23 15:58:11 +0200

    added useParams for grabbing recipe id

--- src/mainpages/EditRecipe.jsx
+++ src/mainpages/EditRecipe.jsx
@@ -1,6 +1,9 @@
 import Content from '../components/Content'
+import { useParams } from 'react-router-dom'
 
 const EditRecipe = () => {
+  const { id } = useParams()
+
   return (
     <Content>
       <div>Edit Recipe</div>

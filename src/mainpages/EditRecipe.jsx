f07f52c1e0e1bd4ff20e729e6d5d2352cdaff64d
Jesse Emana <jesseemana@gmail.com>
2023-08-23 11:51:05 +0200

    created an  edit page for recipes

---
+++ src/mainpages/EditRecipe.jsx
@@ -0,0 +1,11 @@
+import Content from '../components/Content'
+
+const EditRecipe = () => {
+  return (
+    <Content>
+      <div className='py-20'>EditRecipe</div>
+    </Content>
+  )
+}
+
+export default EditRecipe   

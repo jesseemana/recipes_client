e6cd4944321fffbc64c90be029c395791489f08d
Jesse Emana <jesseemana@gmail.com>
2023-08-24 11:34:48 +0200

    changed page layout

--- src/components/ErrorFallback.jsx
+++ src/components/ErrorFallback.jsx
@@ -1,13 +1,9 @@
-import Content from './Content'
-
 function ErrorFallback({ error, resetErrorBoundary }) {
    return (
-    <Content>
-      <div role='alert' className='h-[100vh] flex flex-col items-center justify-center text-center'>
-        <p className='font-bold text-2xl'>Something went wrong:</p>
-        <p className='text-red-500 text-xl'>{error.message}</p>
-      </div>
-    </Content>
+    <div role='alert' className='h-[100vh] flex flex-col items-center justify-center text-center'>
+      <p className='font-bold text-2xl'>Something went wrong:</p>
+      <p className='text-red-500 text-xl'>{error.message}</p>
+    </div>
   )
 }
 

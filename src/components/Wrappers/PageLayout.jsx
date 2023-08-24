e3c466245f7a0fe816e19a76cfebd73334de2015
Jesse Emana <jesseemana@gmail.com>
2023-08-24 15:09:03 +0200

    created a wrapper folder for components wrapping other components

---
+++ src/components/Wrappers/PageLayout.jsx
@@ -0,0 +1,9 @@
+const PageLayout = ({children}) => {
+  return (
+    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8'>
+      {children}
+    </div>
+  )
+}
+
+export default PageLayout 

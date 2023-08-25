44e2923d2f8abb65afe0cebc7b4fb0fb1da0c69c
Jesse Emana <jesseemana@gmail.com>
2023-08-25 16:35:33 +0200

    Typescript baby

---
+++ src/hooks/useBookmark.tsx
@@ -0,0 +1,43 @@
+import axios from '../api/axios'
+import { useMemo } from 'react'
+import { toast } from 'react-hot-toast'
+
+const useBookmark = ({id, auth}: Bookmark) => {
+  const userId = auth?.user._id
+  const token = auth?.access_token
+
+  const bookmarked = useMemo(() => {
+    return auth?.user.bookmarks.includes(id)
+  }, [id, auth])
+
+  const toggleBookmark = async (e) => {
+    e.stopPropagation()
+    try {
+      if (bookmarked) {
+        await axios.delete(`/bookmarks/${id}/${userId}`, {
+          headers: {'Authorization': `Bearer ${token}`}
+        })
+        toast.success('Bookmark removed')
+      } else {
+        await axios.post(`/bookmarks/${id}/${userId}`, {
+          headers: {'Authorization': `Bearer ${token}`}
+        })
+        toast.success('Bookmarked')
+      }
+      // refresh page here
+    } catch (error) {
+      let errorMessage = 'Something went wrong: '
+      if (error instanceof Error)
+        errorMessage += error
+      console.log(error)
+      toast.error('Failed' )
+    }
+  }
+
+  return {
+    bookmarked,
+    toggleBookmark
+  }
+}
+
+export default useBookmark  

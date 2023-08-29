5b638b0e77781a504be493e950e52175fcaa154e
Jesse Emana <jesseemana@gmail.com>
2023-08-29 20:51:39 +0200

    added new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
-import Loader from './components/Loader'
-import Navbar from './components/Navbar/Navbar'
 import Home from './mainpages/Home'
 import Recipe from './mainpages/Recipe'
 import Profile from './mainpages/Profile'
@@ -9,52 +7,59 @@ import NotFound from './mainpages/NotFound'
 import ResetPwd from './mainpages/ResetPwd'
 import AuthUser from './mainpages/AuthUser'
 import Bookmarks from './mainpages/Bookmarks'
+import EditRecipe from './mainpages/EditRecipe'
+import Navbar from './components/Navbar/Navbar'
 import UserRecipes from './mainpages/UserRecipes'
 import RequireAuth from './components/RequireAuth'
 import CreateRecipe from './mainpages/CreateRecipe'
 import ChangePassword from './mainpages/ChangePassword'
+import Loader from './components/Loaders/Loader'
+
 
 import { lazy, Suspense } from 'react'
 import { ErrorBoundary } from 'react-error-boundary'
 import ErrorFallback from './components/ErrorFallback'
+import {ThemeProvider} from './context/ThemeProvider';
 // const Home = lazy(() => import('./main pages/Home'))
 // const Recipe = lazy(() => import('./mainpages/Recipe'))
 // const UserRecipes = lazy(() => import('./mainpages/UserRecipes'))
 // const Profile = lazy(() => import('./mainpages/Profile'))
 
 
-function App(){ 
+function App() {
   return (
     <>
-      <Navbar />
-      <ErrorBoundary FallbackComponent={ErrorFallback}>
-        <Routes>
-          <Route path='/' element={<Navigate to='/feed'/>} />
-          <Route path='/auth' element={<AuthUser />} />
-          <Route path='/feed' element={<Home />} />
-          <Route path='/recipe/:id' element={<Recipe />} />
-          <Route path='/reset' element={<ResetPwd />} />
-          <Route path='/reset-password/:id/:token' element={<ChangePassword />} />
-
-          {/* PROTECTED ROUTES */}
-          <Route element={<RequireAuth />}>
-            <Route path='/auth' element={<Navigate to='/feed'/>} />
-            <Route path='/create' element={<CreateRecipe />} />
-            <Route path='/bookmarks' element={<Bookmarks />} />
-            <Route path='/user/:id' element={<UserRecipes />} />
+      <ThemeProvider>
+        <Navbar />
+        <ErrorBoundary FallbackComponent={ErrorFallback}>
+          <Routes>
+            <Route path='/' element={<Navigate to='/feed'/>} />
+            <Route path='/feed' element={<Home />} />
+            <Route path='/auth' element={<AuthUser />} />
+            <Route path='/reset' element={<ResetPwd />} />
+            <Route path='/recipe/:id' element={<Recipe />} />
+            <Route path='/reset-password/:id/:token' element={<ChangePassword />} />
             <Route path='/profile/:id' element={<Profile />} />
-          </Route>
 
-          <Route path='*' element={<NotFound />} />
-        </Routes>
-      </ErrorBoundary>
-      <Toaster />
+            {/* PROTECTED ROUTES */}
+            <Route element={<RequireAuth />}>
+              <Route path='/bookmarks' element={<Bookmarks />} />
+              <Route path='/edit/:id' element={<EditRecipe />} />
+              <Route path='/create' element={<CreateRecipe />} />
+              <Route path='/user/:id' element={<UserRecipes />} />
+              <Route path='/auth' element={<Navigate to='/feed'/>} />
+            </Route>
+
+            <Route path='*' element={<NotFound />} />
+          </Routes>
+        </ErrorBoundary>
+        <Toaster />
+      </ThemeProvider>
     </>
   )
 }
 
 export default App  
-
 {/* <Route path='/bookmarks' element={<Suspense fallback={<Loader />}> <Bookmarks /> </Suspense>} />
 <Route path='/user/:id' element={<Suspense fallback={<Loader />}> <UserRecipes /> </Suspense>} />
 <Route path='/profile/:id' element={<Suspense fallback={<Loader />}> <Profile /> </Suspense>} /> */}
---
+++ src/components/AuthForm.tsx
@@ -0,0 +1,132 @@
+import { Link } from 'react-router-dom'
+import Button from './Buttons/Button'
+import Heading from './Inputs/Heading'
+import InputField from './Inputs/InputField'
+import { Dispatch, SetStateAction } from 'react'
+
+interface AuthForm {
+  email: string 
+  isLogin: boolean
+  submitting: boolean
+  firstName: string
+  lastName: string
+  password: string
+  confirmPassword: string
+  handleChange: () => void
+  handleSubmit: () => void
+  setEmail: Dispatch<SetStateAction<string>>
+  setFirstName: Dispatch<SetStateAction<string>>
+  setLastName: Dispatch<SetStateAction<string>>
+  setPassword: Dispatch<SetStateAction<string>>
+  setConfirmPassword: Dispatch<SetStateAction<string>>
+}
+
+const Form = ({
+  email, 
+  isLogin,
+  firstName, 
+  lastName, 
+  password, 
+  confirmPassword, 
+  submitting,
+  handleChange,  
+  handleSubmit, 
+  setEmail, 
+  setFirstName, 
+  setLastName, 
+  setPassword, 
+  setConfirmPassword, 
+}: AuthForm) => {
+  return (
+    <div className='flex justify-center items-center bg-gray-50 h-[100vh]'>
+      <form 
+        onSubmit={handleSubmit}
+        className='flex flex-col gap-y-3 transition-all w-[300px] md:w-auto px-5 py-4 rounded-sm shadow-lg bg-white'
+      >
+        {isLogin ? <><Heading label='login' /></> : <><Heading label='create account' /></>}
+    
+        {!isLogin && (
+          <div className='flex flex-col gap-y-4 items-start md:flex-row gap-x-4'>
+            <div className='flex flex-col w-full gap-3'>
+              <InputField 
+                htmlFor='first name'
+                label='first name:'
+                id='first name'
+                type='text'
+                value={firstName}
+                placeholder='first name'
+                onChange={(e) => setFirstName(e.target.value)}
+              />
+            </div>
+                    
+            <div className='flex flex-col w-full gap-3'>
+              <InputField 
+                htmlFor='last name'
+                label='last name:'
+                id='last name'
+                type='text'
+                value={lastName}
+                placeholder='last name'
+                onChange={(e) => setLastName(e.target.value)}
+              />
+            </div>
+          </div>
+        )}
+
+        <InputField 
+          htmlFor='email'
+          label='email:'
+          id='email'
+          type='text'
+          value={email}
+          placeholder='email@example.com'
+          onChange={(e) => setEmail(e.target.value)}
+        />
+            
+        <InputField 
+          htmlFor='password'
+          label='password:'
+          id='password'
+          type='password'
+          value={password}
+          placeholder='password'
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <InputField 
+          htmlFor='confirm password'
+          label='confirm password:'
+          id='confirm password'
+          type='password'
+          value={confirmPassword}
+          placeholder='confirm password'
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
+                
+        <Link to={'/reset'}>
+          {isLogin && <p className='text-[#38D6C4] text-center underline capitalize'>forgot password?</p>}
+        </Link>
+
+        <Button
+          type='submit'
+          disabled={submitting}
+          label={isLogin ? 'login' : 'register'}
+        />
+
+        <div className='flex gap-1'>
+          <div className='text-gray-500 text-sm md:text-[17px]'>
+            {isLogin ? `Don't have an account?`: 'Already have an account?'}
+          </div>
+          <Button
+            type='button'
+            disabled={submitting}
+            onClick={handleChange}
+            label={isLogin ? 'create account.' : 'login.'}
+          />
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default Form    

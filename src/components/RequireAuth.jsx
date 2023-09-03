21026f94c3fe51c8ed97fee8b9579aa17cf46678
Jesse Emana <jesseemana@gmail.com>
2023-09-04 01:22:20 +0200

    changed redirection page for nonlogged in users

--- src/components/RequireAuth.jsx
+++ src/components/RequireAuth.jsx
@@ -9,7 +9,7 @@ const RequireAuth = () => {
   return (
     auth?.user 
       ? <Outlet /> 
-      : <Navigate to={'/auth'} state={{ from: location }} replace/>
+      : <Navigate to={'/login'} state={{ from: location }} replace/>
   )
 }
 

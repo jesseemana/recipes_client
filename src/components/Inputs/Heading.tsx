3df139b9f45e1fae303e7c60f16504658f2d05d7
Jesse Emana <jesseemana@gmail.com>
2023-09-07 09:45:37 +0200

    changed stylings

--- src/components/Inputs/Heading.tsx
+++ src/components/Inputs/Heading.tsx
@@ -1,6 +1,6 @@
 const Heading = ({label}: {label: string}) => {
   return (
-    <h1 className='text-center capitalize text-lg md:text-2xl font-semibold text-[#38D6C4] py-1'>
+    <h1 className='text-center capitalize text-lg md:text-2xl font-semibold text-[#38D6C4]'>
       {label}
     </h1>
   )
--- src/components/Inputs/InputField.tsx
+++ src/components/Inputs/InputField.tsx
@@ -13,7 +13,7 @@ const InputField = (props: InputProps) => {
         placeholder={props.placeholder}
         {...(props.inputProps ?? {})}
         autoComplete='off'
-        className='py-2 px-1 text-gray-600 border border-gray-300 rounded-sm outline-[#38D6C4] file:cursor-pointer file:border-0 file:text-gray-600'
+        className='py-1 md:py-2 px-1 text-sm text-gray-600 border border-gray-300 rounded-sm outline-[#38D6C4] file:cursor-pointer file:border-0 file:text-gray-600'
       />
       {props.error && <span className='text-red-600 text-sm'>{props.error}</span>}
     </>

99414914666c23592464af766325f8c6278fc5d9
Jesse Emana <jesseemana@gmail.com>
2023-09-08 17:09:01 +0200

    removed wrapper div for the loader

--- src/components/Loaders/Loader.tsx
+++ src/components/Loaders/Loader.tsx
@@ -2,13 +2,11 @@ import GridLoader from 'react-spinners/GridLoader'
 
 const Loader = () => {
   return (
-    <div className='grid place-items-center h-[79vh]'>
-      <GridLoader 
-        size={24}
-        color='#38D6C4'
-        aria-label='Loading Spinner' 
-      />
-    </div>
+    <GridLoader 
+      size={24}
+      color='#38D6C4'
+      aria-label='Loading Spinner' 
+    />
   )
 }
 

6817897efc5067846ce4dc05121e7251cec33641
Jesse Emana <jesseemana@gmail.com>
2023-09-08 17:10:56 +0200

    modified padding for button

--- src/components/Buttons/Button.tsx
+++ src/components/Buttons/Button.tsx
@@ -6,7 +6,7 @@ const Button = ({ disabled, onClick, label, type}: ButtonProps) => {
       onClick={onClick}
       className={`disabled:opacity-80 disabled:cursor-not-allowed text-sm cursor-pointer
         ${type === 'submit'
-          ? 'bg-[#38D6C4] text-white uppercase rounded-sm py-2 font-semibold'
+          ? 'bg-[#38D6C4] text-white uppercase rounded-sm py-1 md:py-2 font-semibold'
           : 'text-[#38D6C4] md:text-[17px] underline capitalize'}
       `}
     >

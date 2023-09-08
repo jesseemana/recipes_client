62b42179a67d88e892990adbd3516b7aa7ed8979
Jesse Emana <jesseemana@gmail.com>
2023-09-08 17:08:32 +0200

    modified stylings

--- src/components/Inputs/Header.tsx
+++ src/components/Inputs/Header.tsx
@@ -1,19 +1,16 @@
 interface HeaderProps {
+  center?: boolean
   title: string
   subtitle: string
 }
 
-const Header = ({title, subtitle}: HeaderProps) => {
+const Header = ({title, subtitle, center}: HeaderProps) => {
   return (
-    <div className={`text-start pb-5`}>
-      <div className='text-2xl font-bold capitalize'>
-        {title}
-      </div>
-      <div className='font-normal text-neutral-500 mt-2 text-lg'>
-        {subtitle}
-      </div>
+    <div className={center ? 'text-center pb-5'  : 'text-start pb-5'}>
+      <h1 className='text-2xl text-neutral-800 font-bold capitalize'>{title}</h1>
+      <p className='font-light text-neutral-500 mt-2 text-lg'>{subtitle}</p>
     </div>
   )
 }
 
-export default Header  
+export default Header 

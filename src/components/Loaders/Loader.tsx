95c9f6e329bcf53c78a3e68bc6b88c1cd12b73dd
Jesse Emana <jesseemana@gmail.com>
2023-09-25 23:48:21 +0200

    fixed size

--- src/components/Loaders/Loader.tsx
+++ src/components/Loaders/Loader.tsx
@@ -3,9 +3,8 @@ import GridLoader from 'react-spinners/GridLoader'
 const Loader = () => {
   return (
     <GridLoader 
-      size={24}
       color='#38D6C4'
-      aria-label='Loading Spinner' 
+      aria-label='Loading Spinner'
     />
   )
 }

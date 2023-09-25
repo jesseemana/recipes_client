57f82034a8d3dbb48da11e7764632fe73118237b
Jesse Emana <jesseemana@gmail.com>
2023-09-26 01:01:04 +0200

    changed variable type for custom hook and imported RecipeUI component

--- src/mainpages/Recipe.jsx
+++ src/mainpages/Recipe.jsx
@@ -1,13 +1,12 @@
-import { Link } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import { BsClock } from 'react-icons/bs'
-import { BsBookmark, BsBookmarkFill } from 'react-icons/bs'
+
 import axios from '../api/axios'
-import Loader from '../components/Loaders/Loader'
+import Loader from '@/components/Loaders/Loader'
 import useAuth from '../hooks/useAuth'
 import useBookmark from '../hooks/useBookmark'
 import useDocumentTitle from '../hooks/useDocumentTitle'
+import RecipeUI from '@/components/RecipeUI'
 
 const Recipe = () => {
   const { id } = useParams()
@@ -18,98 +17,56 @@ const Recipe = () => {
 
   const [owner, setOwner] = useState('')
   const [recipe, setRecipe] = useState({})
+  const [pageName, setPageName] = useState('')
   const [loading, setLoading] = useState(false)
 
-  useDocumentTitle(loading ? 'loading...' : recipe.name)
+  useDocumentTitle(loading ? 'loading...' : pageName.toUpperCase())
 
-  const { bookmarked, toggleBookmark } = useBookmark({id, auth})
-
-  const getRecipe = async () => {
-    setLoading(true)
-    try {
-      const response = await axios.get(`recipes/${id}`, { 
-        headers: {'Authorization': `Bearer ${token}`}
-      })
-      if (response.data)
-        setRecipe(response.data.recipe)
-        setOwner(response.data.full_name)
-    } catch(error) {
-      let errorMessage = 'Something went wrong: '
-      if (error instanceof Error)
-        errorMessage += error
-      console.log(error)
-    } finally {
-      setLoading(false)
-    }
+  if (token) {
+    var { bookmarked, toggleBookmark } = useBookmark({id, auth})
   }
-  
+
   useEffect(() => {
+    const getRecipe = async () => {
+      setLoading(true)
+      try {
+        const response = await axios.get(`recipes/${id}`, { 
+          headers: {'Authorization': `Bearer ${token}`}
+        })
+        if (response.data)
+          setRecipe(response.data.recipe)
+          setOwner(response.data.owner)
+          setPageName(response.data.recipe.name)
+      } catch(error) {
+        let errorMessage = 'Something went wrong: '
+        if (error instanceof Error)
+          errorMessage += error
+        console.log(error)
+      } finally {
+        setLoading(false)
+      }
+    }
+  
     getRecipe()
   }, []) // eslint-disable-line react-hooks/exhaustive-deps
+
+  console.log(bookmarked)
 
   let content
 
   if (loading) {
-    content = (
+    return <div className='h-[100vh] grid place-items-center'>
       <Loader />
-    )
+    </div> 
   }
 
   content = (
-    <div className='max-w-full px-[8%] flex flex-col justify-center gap-y-5 gap-x-20 md:flex-row py-4'>
-      <div className='flex flex-col gap-y-4'>
-        <img 
-          src={recipe.picture_path} 
-          alt={recipe.name}
-          className='w-[300px] h-[250px] lg:h-[390px] lg:w-[500px] shadow-lg rounded-md'
-        />
-        <h1 className='capitalize font-semibold text-xl'>{recipe.name}</h1>
-        <div className='flex gap-x-5'>
-          <p className='flex items-center gap-x-2 text-gray-700'>
-            <span><BsClock  className='text-xl text-[#38D6C4]'/></span>
-            {recipe.time}min
-          </p>
-          {!token ? 
-            <div className='flex items-center gap-x-2 text-gray-700'>
-              <span><BsBookmark className='text-xl text-[#38D6C4]' /></span>
-              login to save
-            </div> : 
-            <>
-              {!bookmarked ? 
-                <button
-                  onClick={toggleBookmark}
-                  className='flex items-center gap-x-2 text-gray-700'
-                >
-                  <span><BsBookmark className='text-xl text-[#38D6C4]' /></span>
-                  save recipe
-                </button> : 
-                <button
-                  onClick={toggleBookmark}
-                  className='flex items-center gap-x-2 text-gray-700'
-                >
-                  <span><BsBookmarkFill className='text-xl fill-[#38D6C4]' /></span>
-                  saved
-                </button>
-              }
-            </>
-          }
-        </div>
-        <Link 
-          to={`/user/${recipe.user}`} 
-          className='text-md font-extralight w-[190px] text-gray-500'
-        >
-          view more by <span className='text-[#38D6C4] font-normal underline'>{owner}</span>
-        </Link>
-        <div className='border border-l-0 border-r-0 border-b-0 pt-3 '>
-          <h1 className='uppercase text-lg font-medium text-gray-900'>ingridients</h1>
-          <p className='max-w-[400px] text-gray-600'>{recipe.ingridients}</p>
-        </div>
-      </div>
-      <div>
-        <h1 className='uppercase text-lg font-medium text-gray-900 border py-3 border-l-0 border-r-0'>procedure</h1>
-        <p className='max-w-[400px] text-gray-600'>{recipe.procedure}</p>
-      </div>
-    </div>
+    <RecipeUI
+      recipe={recipe}
+      owner={owner}
+      bookmarked={bookmarked}
+      toggleBookmark={toggleBookmark}
+    />
   )
 
   return content

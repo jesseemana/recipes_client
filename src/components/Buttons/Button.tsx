8d4d10aea041a5896137620199f7fc513cde313e
Jesse Emana <jesseemana@gmail.com>
2023-10-02 10:44:23 +0200

    changed some stylings

--- src/components/Buttons/Button.tsx
+++ src/components/Buttons/Button.tsx
@@ -7,7 +7,7 @@ const Button = ({ disabled, onClick, label, type}: ButtonProps) => {
       className={`disabled:opacity-80 disabled:cursor-not-allowed text-sm cursor-pointer
         ${type === 'submit'
           ? 'bg-[#38D6C4] text-white uppercase rounded-sm py-1 md:py-2 font-semibold'
-          : 'text-[#38D6C4] md:text-[17px] underline capitalize'}
+          : 'text-[#38D6C4] md:text-[15px] underline capitalize'}
       `}
     >
       {label}

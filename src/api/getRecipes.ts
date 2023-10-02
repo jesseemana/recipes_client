27cbff02de7de804d7536d7e1fbec28c11b2d13b
Jesse Emana <jesseemana@gmail.com>
2023-10-02 18:32:00 +0200

    recipes fetching function

---
+++ src/api/getRecipes.ts
@@ -0,0 +1,20 @@
+import { z } from 'zod'
+import axios from './axios'
+import { RecipeSchema } from '@/model/model'
+
+const RecipeResults = z.array(RecipeSchema)
+
+type RecipeArray = z.infer<typeof RecipeResults>
+
+export async function getRecipes({currentPage = 1}: { currentPage: number }): Promise<RecipeArray | undefined> {
+    try {
+        const response = await axios.get(`/recipes?page=${currentPage}`)
+        const data = await response.data
+        return data
+    } catch (error: unknown) {
+        let errorMessage = 'Something went wrong: '
+        if (error instanceof Error)
+          errorMessage += error
+        console.log(errorMessage)
+    }
+}   

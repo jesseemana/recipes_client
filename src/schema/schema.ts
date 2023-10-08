cd91e4deede06e164e5af7c4a8e623e2da04facd
Jesse Emana <jesseemana@gmail.com>
2023-10-08 14:57:11 +0200

    created more error messages

--- src/schema/schema.ts
+++ src/schema/schema.ts
@@ -1,29 +1,36 @@
-import { z } from 'zod'
+import { object, string, number } from 'zod'
 
-export const RecipeSchema = z.object({
-  category: z.string(),
-  ingridients: z.string().trim(),
-  procedure: z.string().min(30).trim(),
-  name: z.string().min(3).max(20).trim(),
-  time: z.string().min(6).max(10).toLowerCase().trim(),
-  total_pages: z.number().optional()
+export const RecipeSchema = object({
+  category: string(),
+  ingridients: string().nonempty({ message: 'Provide the ingridients' })
+    .trim().toLowerCase(),
+  procedure: string().nonempty({ message: 'Procedure cannot be empty' })
+    .min(30).trim(),
+  name: string().nonempty({ message: 'Please, rovide the name' })
+    .min(3).max(20).toLowerCase().trim(),
+  time: string().nonempty({ message: 'Field cannot be empty' })
+    .min(6).max(10).toLowerCase().trim(),
+  total_pages: number().optional()
 })
 
-export const AuthSchema = z.object({
-  first_name: z.string()
+export const AuthSchema = object({
+  first_name: string().nonempty({ message: 'Provide a first name' })
     .min(3, 'Must be 3 at least characters long')
-    .max(24, 'Name cannot contain more thamn 24 characters').trim().optional(),
-  last_name: z.string()
+    .max(24, 'Name cannot contain more thamn 24 characters')
+    .toLowerCase().trim().optional(),
+  last_name: string().nonempty({ message: 'Provide a last name' })
     .min(3, 'Must be 3 at least characters long')
-    .max(24, 'Name cannot contain more thamn 24 characters').trim().optional(),
-  email: z.string().email('Please enter a valid email').toLowerCase().trim().optional(),
-  password: z.string()
+    .max(24, 'Name cannot contain more thamn 24 characters')
+    .toLowerCase().trim().optional(),
+  email: string().nonempty({ message: 'Provide an email' })
+    .email('Please enter a valid email').toLowerCase().trim().optional(),
+  password: string().nonempty({ message: 'Password is required' })
     .regex(new RegExp(".*[A-Z].*"), "Password must contain at least one uppercase character")
     .regex(new RegExp(".*[a-z].*"), "Password must contain at least one lowercase character")
     .regex(new RegExp(".*\\d.*"), "Password must contain at least one number")
     .regex(new RegExp(".*[`~<>?,./!@#$%^&*()\\-_+=\"'|{}\\[\\];:\\\\].*"), "Password must contain at least one special character")
     .min(8, "Password must be at least 8 characters long").optional(),
-  confirm_password: z.string()
+  confirm_password: string().nonempty({ message: 'Confirm Password is required' })
     .regex(new RegExp(".*[A-Z].*"), "Password must contain at least one uppercase character")
     .regex(new RegExp(".*[a-z].*"), "Password must contain at least one lowercase character")
     .regex(new RegExp(".*\\d.*"), "Password must contain at least one number")
@@ -33,4 +40,4 @@ export const AuthSchema = z.object({
     message: `Passwords don't match`,
     path: ['confirm_password']
   }
-)   
+)     

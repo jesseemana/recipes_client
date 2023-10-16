1232d3458497dafe76225a0e114f7e819505ea75
Jesse Emana <jesseemana@gmail.com>
2023-10-16 11:22:40 +0200

    using custom data fetching hook

--- src/mainpages/Home.jsx
+++ src/mainpages/Home.jsx
@@ -1,12 +1,38 @@
-import GetRecipes from '@/api/GetRecipes'
+import Feed from './Feed'
+import { useEffect } from 'react'
 import Content from '@/components/Wrappers/Content'
+import useFetchRecipes from '@/hooks/useFetchRecipes'
+import PageLayout from '@/components/Wrappers/PageLayout'
+import SkeletonCard from '@/components/Loaders/SkeletonCard'
 
 const Home = () => {
+  const { loading, user, recipes, currentPage, totalPages, getRecipes, setCurrentPage } = useFetchRecipes()
+
+  useEffect(() => {
+    getRecipes()
+  },[])
+
+  if (loading) return (
+    <Content>
+      <PageLayout>
+        {...new Array(15).fill(0).map((item, index) => (
+          <SkeletonCard key={index} />
+        ))}
+      </PageLayout>
+    </Content>
+  )
+
   return (
     <Content>
-      <GetRecipes />
+      <Feed 
+        user={user}
+        recipes={recipes}
+        pages={totalPages}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage} 
+      />
     </Content>
   )
 }
 
-export default Home 
+export default Home     

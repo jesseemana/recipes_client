43c4b2bf8da685651d527ee2800164e7857a4b11
Jesse Emana <jesseemana@gmail.com>
2023-10-17 06:42:05 +0200

    changed hegiht of loading card

--- src/components/Loaders/SkeletonCard.tsx
+++ src/components/Loaders/SkeletonCard.tsx
@@ -2,8 +2,8 @@ import { SkeletonLoader } from "@/components/ui/skeleton"
 
 const SkeletonCard = () => {
   return (
-    <div className='flex flex-col mb-2 gap-3'>
-      <SkeletonLoader className='h-[320px] md:h-[230px] w-full' />
+    <div className='flex flex-col mb-2 gap-2'>
+      <SkeletonLoader className='h-[320px] md:h-[200px] w-full' />
       <SkeletonLoader className='h-4 w-[200px]' />
       <SkeletonLoader className='h-4 w-[100px]' />
       <SkeletonLoader className='h-4 w-[170px]' />
